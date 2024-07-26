c18259d41d1dfe9ee9c1c4765e81e12949d75f77
pooja <pooja@gamil.com>
2024-07-26 15:00:09 +0530

    server change1

---
+++ middleware/protected.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken")
+
+exports.userProtected = (req, res, next) => {
+    const { user } = req.cookies
+    if (!user) {
+        return res.status(404).json({ message: "No Cookie Found" })
+    }
+    jwt.verify(user, process.env.JWT_KEY, (err, decode) => {
+        if (err) {
+            return res.status(404).json({ message: "JET ERROR", error: err.messagge })
+        }
+        req.loggedInUser = decode.userId
+        nex()
+    })
+}
